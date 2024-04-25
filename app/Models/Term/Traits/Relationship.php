a951a68122d7938d1b161e6226358d8690cc8592
Sean Ciaschi <sciaschi1@gmail.com>
2024-04-25 16:06:45 -0400

    Added comments to define returns from functions

--- app/Models/Term/Traits/Relationship.php
+++ app/Models/Term/Traits/Relationship.php
@@ -5,25 +5,32 @@ namespace App\Models\Term\Traits;
 use App\Models\Term\TermCategory\Link\TermCategoryLink;
 use App\Models\Term\TermTag\Link\TermTagLink;
 use App\Models\TermLink\TermLink;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait Relationship
 {
     /**
      * Get all the links for the Relationship
+     * @return HasMany
      */
     public function links()
     {
         return $this->hasMany(TermLink::class, 'term_id', 'id');
     }
 
+    /**
+     * @return HasMany
+     */
     public function tags()
     {
         return $this->hasMany(TermTagLink::class, 'term_id', 'id');
     }
 
+    /**
+     * @return HasMany
+     */
     public function categories()
     {
         return $this->hasMany(TermCategoryLink::class, 'term_id', 'id');
     }
-
 }
